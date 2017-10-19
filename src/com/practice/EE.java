5d3042d05f034b4c94692009057c9caefdb1cfd5
Zach <zgherridge@gmail.com>
2017-10-19 15:58:05 -0500

    Added some other problems.

---
+++ src/com/practice/EE.java
@@ -0,0 +1,107 @@
+package com.practice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Zach on 10/18/2017.
+ */
+public class EE {
+
+
+    private static Map<String, Person> persons = new HashMap<>();
+
+    public static Person get(String name){
+        return persons.computeIfAbsent(name, Person::new);
+    }
+
+    public static boolean check(List<Person> persons){
+        for (Person base : persons) {
+            for (Person other : persons) {
+                if (base.equals(other)) continue;
+
+                boolean directFriend = base.getFriends().contains(other);
+                boolean eoe = base.getEnemies().stream().map(Person::getEnemies).anyMatch(eoeSet -> eoeSet.contains(other));
+                if (!directFriend && !eoe) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static void parse(String in, boolean friends){
+        String[] names = in.split(" ");
+        for (String name : names) {
+            Person person = get(name);
+            for (String s : names) {
+                if (s.equals(name)) continue;
+                (friends ? person.getFriends() : person.getEnemies()).add(get(s));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.next();
+        String line = scanner.nextLine();
+        while (line.startsWith("f") || line.startsWith("e")){
+            String collect = Arrays.stream(line.replaceAll(" +", " ").trim().split(" ")).skip(1).collect(Collectors.joining(" "));
+
+            line = scanner.nextLine();
+        }
+
+
+
+        parse("Tom Fay", false);
+        parse("Fay Mae Joe", true);
+        parse("Al Mae", true);
+        parse("Sam Tom", false);
+        parse("Tom Al", false);
+        parse("Sam Mae", true);
+
+        System.out.println(check(Arrays.asList(get("Mae"), get("Fay"), get("Joe"))));
+        System.out.println(check(Arrays.asList(get("Sam"), get("Al"), get("Fay"))));
+        System.out.println(check(Arrays.asList(get("Joe"), get("Al"))));
+        System.out.println(check(Arrays.asList(get("Fay"), get("Sam"), get("Tom"))));
+    }
+
+    public static class Person {
+
+        private String name;
+        private Set<Person> friends = new HashSet<>();
+        private Set<Person> enemies = new HashSet<>();
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public Set<Person> getFriends() {
+            return friends;
+        }
+
+        public Set<Person> getEnemies() {
+            return enemies;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (!(object instanceof Person)) return false;
+
+            Person person = (Person) object;
+
+            return name != null ? name.equals(person.name) : person.name == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return name != null ? name.hashCode() : 0;
+        }
+    }
+
+
+}
---
+++ src/com/practice/Stenography.java
@@ -0,0 +1,23 @@
+package com.practice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by Zach on 10/10/2017.
+ */
+public class Stenography {
+
+    private static final String ALPHABET =
+            " " + IntStream.rangeClosed('A', 'Z').mapToObj(value -> "" + (char) value).collect(Collectors.joining("")) + ".?\n";
+
+    public static void main(String[] args) {
+        int[] inputs = new int[]{31, 0, 33, 61, 62, 117, 30, 93, 121, 14, 29, 39, 50, 30, 2, 65, 88, 59};
+
+        for (int input : inputs) {
+            System.out.print(ALPHABET.charAt(input % 30));
+        }
+    }
+}
