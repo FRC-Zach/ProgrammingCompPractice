808d105c7f3dc0a66ef8e3e6e22feee8709084d4
Zachary Herridge <zgherridge@gmail.com>
2017-10-27 10:17:50 -0500

    idk

---
+++ src/com/practice/examples/StreamsIntegers.java
@@ -0,0 +1,18 @@
+package com.practice.examples;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by Zachary Herridge on 10/27/2017.
+ */
+public class StreamsIntegers {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
--- src/com/practice/pratice_comp/ResRating.java
+++ src/com/practice/pratice_comp/ResRating.java
@@ -1,33 +1,50 @@
 package com.practice.pratice_comp;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ResRating {
 
-    public static void main(String[] args) {
-
-
-        int critics = 2;
-        int score = 7;
-
-        Set<List<Integer>> checked = new HashSet<>();
-
-        for (int i = 0; i <= score; i++) {
-            for (int j = 0; j <= score; j++) {
-
-                if (i + j <= score){
-                    List<Integer> objects = new ArrayList<>();
-                    objects.add(i);
-                    objects.add(j);
-                    checked.add(objects);
-                }
+    public static void rec(int score, int count, int[] value, Set<List<Integer>> set){
+        IntStream.rangeClosed(0, score).forEach(value1 -> {
+            value[count - 1] = value1;
+            if (count - 1 <= 0) {
+                set.add(IntStream.of(value).boxed().collect(Collectors.toList()));
             }
-        }
+            else {
+                rec(score, count - 1, value, set);
+            }
+        });
+    }
 
-        System.out.println(checked.size());
+    public static void calc(int criticsCount, int[] ratings){
+        Set<List<Integer>> set = new HashSet<>();
+
+        int score = IntStream.of(ratings).sum();
+        rec(score, criticsCount, new int[criticsCount], set);
+        Set<List<Integer>> collect = set.parallelStream()
+                .filter(integers -> {
+                    int sum = integers.stream().mapToInt(Integer::intValue).sum();
+                    if (sum == score){
+                        if (integers.equals(IntStream.of(ratings).boxed().collect(Collectors.toList()))){
+                            return false;
+                        }
+                        for (int i = 0; i < criticsCount; i++) {
+                            if (integers.get(i) > ratings[i]) return true;
+                        }
+                    }
+                    return sum < score;
+                })
+                .collect(Collectors.toSet());
+        System.out.println(collect.size());
+    }
+
+    public static void main(String[] args) {
+        calc(2, new int[]{4, 3});
     }
 
 }
