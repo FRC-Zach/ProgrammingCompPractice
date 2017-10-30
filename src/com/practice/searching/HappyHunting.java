756b6b23a2e114c20e6be945b696faed0c42d052
Zach Herridge <zgherridge@gmail.com>
2017-10-30 16:47:18 -0500

    Added case count.

--- src/com/practice/searching/HappyHunting.java
+++ src/com/practice/searching/HappyHunting.java
@@ -10,20 +10,25 @@ import java.util.stream.Collectors;
 public class HappyHunting {
 
     public static int loc = 50;
-    private static List<Integer> locations = new ArrayList<>(Arrays.asList(56, 3, 8, 82, 203));
+    public static int caseCount = 1;
 
-    public static void main(String[] args) {
+    public static void search(List<Integer> locations){
         StringJoiner result = new StringJoiner(" ");
+        result.add("Case "  + (caseCount++) + ":");
         while (locations.size() > 0){
             locations.sort(Comparator.comparingInt(location -> Math.abs(loc - location)));
-            
+
             Integer nextEgg = locations.remove(0);
             locations.remove(nextEgg);
             loc = nextEgg;
             result.add(String.valueOf(nextEgg));
         }
-
         System.out.println(result.toString());
+    }
+
+    public static void main(String[] args) {
+        search(new ArrayList<>(Arrays.asList(20, 32, 10, -1)));
+        search(new ArrayList<>(Arrays.asList(56, 3, 8, 82, 203)));
     }
 
 }
