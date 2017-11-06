f1e801bade1d54740b8326e5a58fe519900349e0
Zach Herridge <zgherridge@gmail.com>
2017-11-06 11:38:46 -0600

    Clean up

--- src/com/practice/searching/HappyHunting.java
+++ src/com/practice/searching/HappyHunting.java
@@ -1,25 +1,23 @@
 package com.practice.searching;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * Created by Zachary Herridge on 10/30/2017.
  */
 public class HappyHunting {
 
-    public static int loc = 50;
+    public static int loc;
     public static int caseCount = 1;
 
-    public static void search(List<Integer> locations){
+    public static void search(int startLocation, List<Integer> locations){
+        loc = startLocation;
         StringJoiner result = new StringJoiner(" ");
         result.add("Case "  + (caseCount++) + ":");
         while (locations.size() > 0){
             locations.sort(Comparator.comparingInt(location -> Math.abs(loc - location)));
 
             Integer nextEgg = locations.remove(0);
-            locations.remove(nextEgg);
             loc = nextEgg;
             result.add(String.valueOf(nextEgg));
         }
@@ -27,8 +25,7 @@ public class HappyHunting {
     }
 
     public static void main(String[] args) {
-        search(new ArrayList<>(Arrays.asList(20, 32, 10, -1)));
-        search(new ArrayList<>(Arrays.asList(56, 3, 8, 82, 203)));
+        search(14, new ArrayList<>(Arrays.asList(20, 32, 10, -1)));
+        search(50, new ArrayList<>(Arrays.asList(56, 3, 8, 82, 203)));
     }
-
 }
